1a71d4ba11d6140bddeb71ff57b9ae93cf74603b
mike.ball@mikutech.com <mike.ball@mikutech.com>
2017-02-16 16:26:21 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+import * as nwgui from "nw.gui";
+import * as dimensions from "./const/dimensions";
+import * as Keycodes from "./const/keycodes";
+import * as GameClock from "./providers/GameClock";
+import * as Keyboard from "./providers/Keyboard";
+import * as Mouse from "./providers/Mouse";
+import * as Stage from "./providers/Stage";
+
+// tslint:disable-next-line:interface-name
+interface Window extends NWJS_Helpers.win {
+    reload: (arg?: number) => void;
+}
+
+function main() {
+    let win = <Window> nwgui.Window.get();
+
+    // win.enterFullscreen();
+    let renderer: PIXI.SystemRenderer = new PIXI.WebGLRenderer(dimensions.SCREEN_WIDTH,
+                                                               dimensions.SCREEN_HEIGHT,
+                                                               { backgroundColor : 0x1099bb });
+    renderer.view.classList.add("scaling");
+    renderer.view.id = "stage";
+    (document.querySelector("#canvas-screen") || new Element()).appendChild(renderer.view);
+    let stage: PIXI.Container = new PIXI.Container();
+    // win.addListener("focus", e => renderer.view.requestPointerLock());
+    // renderer.view.requestPointerLock();
+    // renderer.view.addEventListener("mousemove", e => console.log(e.movementX, e.movementY));
+
+    Mouse.Init(window, renderer.view);
+    GameClock.Init(30);
+    Keyboard.Init(document.body);
+    Stage.Init(win, renderer, stage);
+
+    // global hotkeys
+    // escape closes the window
+    Keyboard.provider().do(() => win.close(), (key) => key.pressed && key.keyCode === Keycodes.ESCAPE);
+    // F12 opens dev tools
+    Keyboard.provider().do(() => win.showDevTools(), (key) => key.pressed && key.keyCode === Keycodes.F12);
+    // F11 reloads the window
+    Keyboard.provider().do(() => win.reload(3), (key) => !key.pressed && key.keyCode === Keycodes.F11);
+    // ALT + F toggles fullscreen
+    Keyboard.provider().do(() => win.toggleFullscreen(), (key) => key.pressed && key.keyCode === Keycodes.F &&
+                                                                Keyboard.isKeyDown(Keycodes.ALT));
+    let debugVisible = true;
+    // F10 toggles debug info
+    Keyboard.provider().do(() => (<HTMLParagraphElement> document.querySelector("#info")).style.visibility =
+                                                            (debugVisible = !debugVisible) ? "visible" : "hidden",
+        (key) => key.pressed && key.keyCode === Keycodes.F10);
+
+    // frame rendering uses requestAnimationFrame for acceleration
+    let frameTimes: number[] = [];
+    let tick: () => void;
+    requestAnimationFrame(tick = () => {
+        renderer.render(stage);
+        requestAnimationFrame(tick);
+    });
+}
